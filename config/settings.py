83ffe520bedc8a69996474163ec78cc8cf4a9d32
Denis <seltix9381@gmail.com>
2024-07-01 19:14:54 +0300

    + update 3

--- config/settings.py
+++ config/settings.py
@@ -89,7 +89,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+# settings.py
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
