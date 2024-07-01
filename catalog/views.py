9248a1f7b0083a1cccd5b675e50d92bb179a77d2
Denis <seltix9381@gmail.com>
2024-07-01 19:25:13 +0300

    + update 4

--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        phone = request.POST.get('phone')
+        print(name, message, phone)
+    return render(request, 'contacts.html')
